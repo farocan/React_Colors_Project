f790d604266d136993c1641bcaca30684b88c5bf
farocan <anna.fr.1990@gmail.com>
2024-02-19 08:45:38 +0100

    adding MiniPalette

---
+++ colors-app/src/MiniPalette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./MiniPalette.css";
+// import { withStyles } from '@material-ui/core/styles';
+
+
+function MiniPalette(){
+    return (
+        <div className='title'>
+            <h1>Mini Palette</h1>
+        </div>
+    );
+}
+export default (MiniPalette);
